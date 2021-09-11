031f7eb7b866b7ca6fa04762bc689ca01d28394e
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-09-12 01:14:41 +0700

    feat: store data in smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint totalWaves;
   constructor() {
     console.log("Hello, this is a smart contract");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s is waved", msg.sender);
+  }
+
+  function getTotalWaves() view public returns (uint) {
+    console.log("We have %d total waves", totalWaves);
+    return totalWaves;
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,27 @@
 const hre = require("hardhat");
 
 const main = async () => {
+  const [owner, randoPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  // simulate others
+  waveTxn = await waveContract.connect(randoPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 main()
